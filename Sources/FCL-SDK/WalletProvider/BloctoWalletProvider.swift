b8061fe3c1885d7327cf1534ba691961ca5d71bb
Chase McDermott <chase.mcdermott12@gmail.com>
2023-02-21 22:13:25 -0600

    Use limit arg when submitting a tx

--- Sources/FCL-SDK/WalletProvider/BloctoWalletProvider.swift
+++ Sources/FCL-SDK/WalletProvider/BloctoWalletProvider.swift
@@ -204,6 +204,7 @@ public final class BloctoWalletProvider: WalletProvider {
                 script: Data(cadence.utf8),
                 arguments: arguments,
                 referenceBlockId: block.blockHeader.id,
+                limit: limit,
                 proposalKey: proposalKey,
                 payer: feePayer,
                 authorizers: authorizers
